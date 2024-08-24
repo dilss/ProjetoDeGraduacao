40e5ca01b5edaf3211e8111a0215eb710e40b0ac
edilson <edilson.fernandes@poli.ufrj.br>
2024-08-24 17:38:58 -0300

    backend: adds model for the sensors received payload

--- IMRSAC/backend/app/src/main/java/com/imrsac/mqtt/MqttMirrigaConsumer.java
+++ IMRSAC/backend/app/src/main/java/com/imrsac/mqtt/MqttMirrigaConsumer.java
@@ -2,7 +2,6 @@ package com.imrsac.mqtt;
 
 import java.nio.charset.StandardCharsets;
 import java.util.Base64;
-import java.util.Map;
 import java.util.concurrent.CompletionStage;
 
 import org.eclipse.microprofile.reactive.messaging.Incoming;
@@ -10,9 +9,9 @@ import org.eclipse.microprofile.reactive.messaging.Message;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 
 import jakarta.enterprise.context.ApplicationScoped;
 
@@ -22,14 +21,23 @@ public class MqttMirrigaConsumer {
     private static final Logger LOG = LoggerFactory.getLogger(MqttMirrigaConsumer.class);
 
     @Incoming("mirriga")
-
-    public CompletionStage<Void> consume(Message<byte[]> message) throws JsonMappingException, JsonProcessingException {
+    public CompletionStage<Void> consume(Message<byte[]> message) {
         LOG.info("Novo Pacote Recebido via MQTT");
-        String payloadString = new String(message.getPayload(), StandardCharsets.UTF_8);
-        ObjectMapper mapper = new ObjectMapper();
-        Map<String,Object> map = mapper.readValue(payloadString, Map.class);
-        var value = Base64.getDecoder().decode(map.get("data").toString());
-        System.out.println("Dado: " + new String(value, StandardCharsets.UTF_8));
-        return message.ack();
+
+        try {
+            String payloadString = new String(message.getPayload(), StandardCharsets.UTF_8);
+            ObjectMapper mapper = new ObjectMapper();
+            mapper.registerModule(new JavaTimeModule());
+            mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+            SensorPayload payload = mapper.readValue(payloadString, SensorPayload.class);
+            String value = new String(Base64.getDecoder().decode(payload.getData()), StandardCharsets.UTF_8);
+            LOG.info("Dados recebidos do sensor \"{}\" - Umidade do solo: {}", payload.getDeviceInfo().getDeviceName(),
+                    value);
+            return message.ack();
+
+        } catch (Exception e) {
+            LOG.error("Falha ao extrair dados da mensagem. Motivo: {}", e.getMessage());
+            return message.nack(e);
+        }
     }
 }
